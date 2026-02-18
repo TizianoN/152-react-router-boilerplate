6739670be5ebf906ddb7fe7f92a9d91ab36a7a05
TizianoN <hello@tizianonicolai.com>
2026-02-18 12:10:19 +0100

    add: about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index Component={HomePage} />
+        <Route path="/about-us" Component={AboutPage} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+import Header from "../components/Header";
+
+export default function AboutPage() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <div className="py-5 container">
+          <h1>About this project</h1>
+          <p className="text-muted">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae minima
+            voluptas ea repellendus aliquid, expedita nulla iure eligendi!
+            Dignissimos reiciendis nemo aliquid praesentium, soluta eos
+            assumenda sapiente veniam dicta cumque.
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
