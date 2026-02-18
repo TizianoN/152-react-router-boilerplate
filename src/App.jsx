88e399475ff86fe43c251224366465e80d4777fe
TizianoN <hello@tizianonicolai.com>
2026-02-18 12:33:56 +0100

    add: default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index Component={HomePage} />
-        <Route path="/about-us" Component={AboutPage} />
+        <Route Component={DefaultLayout}>
+          <Route index Component={HomePage} />
+          <Route path="/about-us" Component={AboutPage} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,9 @@
-import Header from "../components/Header";
-
 export default function HomePage() {
   return (
     <>
-      <Header />
-
-      <main>
-        <div className="py-5 container">
-          <h1>React router boilerplate</h1>
-        </div>
-      </main>
+      <div className="py-5 container">
+        <h1>React router boilerplate</h1>
+      </div>
     </>
   );
 }
